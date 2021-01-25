a436c5c2b8fbfc574e83f7d2a587a184c341b0be
Christophe Meessen <christophe@meessen.net>
2021-01-25 14:36:49 +0100

    add CLI options for version and help message

--- main.c
+++ main.c
@@ -5,16 +5,48 @@
 #include <stdlib.h> 
 #include <fcntl.h>
 #include <assert.h>
+#include <stdbool.h>
+#include <string.h>
 #include "qjson.h"
 #ifdef __AFL_COMPILER 
 #include <json.h>
 #endif
 
 
+void printHelp(FILE *out) {
+    fprintf(out, "Usage: qjson <qjson file> | -v | -? | --help\n");
+    fprintf(out, "Print the qjson file content converted to JSON to stdout and return 0. "
+    "When the qjson is invalid, print an error message to stderr and return 1.\n");
+    fprintf(out, "  -v           outputs the version.\n");
+    fprintf(out, "  -?, --help   outputs this help message.\n");
+    fprintf(out, "\nReturn status is 0 when the convertion was successful, 1 otherwise\n");
+}
+
+
+bool argsContain(int argc, char *argv[], const char *val) {
+    for (int i = 0; i < argc; i++) {
+        if (strcmp(argv[i], val) == 0)
+            return true;
+    }
+    return false;
+}
+
+
 int main(int argc, char *argv[]) {
-    if (argc < 2){
-        fprintf(stderr, "error: require a file name as argument\n");
+    if (argc == 1) {
+        fprintf(stderr, "error: require a file name or an option as argument\n");
+        printHelp(stderr);
         return 1;
+    }
+
+    if (argsContain(argc, argv, "-?") || argsContain(argc, argv, "--help")) {
+        printHelp(stdout);
+        return 0;
+    }
+
+    if (argsContain(argc, argv, "-v")) {
+        printf("%s\n", qjson_version());
+        return 0;
     }
 
     const char *fileName = argv[1];
